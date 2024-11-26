0933ea33b04735a5f63580677106e804c4842bd4
Jorge Ross <jorg.ross.13@gmail.com>
2024-11-25 18:28:11 -0600

    -rloc-img-2

--- src/components/data.js
+++ src/components/data.js
@@ -1,10 +1,8 @@
 export const projects = [
   {
     id: "1",
-    imgSrc:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/get-that-home.png",
-    imgComp:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/gth-cpm.png",
+    imgSrc: "/images/get-that-home.png",
+    imgComp: "/images/gth-cpm.png",
     title: "Get that home",
     summary:
       "A sophisticated React-based application meticulously designed to provide a compelling, user-friendly platform for renting, selling, and searching for houses and apartments. This application integrates advanced features such as price range filters for rooms and bathrooms, allowing users to tailor their search to fit their specific needs. Additionally, the intuitive interface ensures a seamless experience, making it easy for users to navigate through listings, view property details, and contact sellers or landlords. Whether users are looking to find their ideal home or list their properties with ease, this application offers a comprehensive and efficient solution for all real estate needs.",
@@ -12,33 +10,28 @@ export const projects = [
     team: [
       {
         name: "Gabriel Nuñez",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/gab.jpg",
+        profilePicUrl: "/images/gab.jpg",
         githubUrl: "https://github.com/NunezGabriel",
       },
       {
         name: "Jorge Rosano",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/jrh.jpg",
+        profilePicUrl: "/images/jrh.jpg",
         githubUrl: "https://github.com/jorge-ross",
         linkedinurl: "https://www.linkedin.com/in/jorgeros13",
       },
       {
         name: "Pedro Arias",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/ped.png",
+        profilePicUrl: "/images/ped.png",
         githubUrl: "https://github.com/pdroariasvilela",
       },
       {
         name: "Diego Gómez",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/dg.jpg",
+        profilePicUrl: "/images/dg.jpg",
         githubUrl: "https://github.com/DiegoGomezCuadross",
       },
       {
         name: "Daniel Toledo",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/dt.jpg",
+        profilePicUrl: "/images/dt.jpg",
         githubUrl: "https://github.com/DanielToledo-99",
       },
     ],
@@ -46,10 +39,8 @@ export const projects = [
   },
   {
     id: "2",
-    imgSrc:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/ndev.png",
-    imgComp:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/logond-wb.png",
+    imgSrc: "/images/ndev.png",
+    imgComp: "/images/logond-wb.png",
     title: "Neuro-dev",
     summary:
       "Explore my bespoke clinic website, which aims to serve as a comprehensive psychology platform. Although still in progress, it is designed to deliver a seamless and intuitive user experience for those seeking mental health care. The website will offer a range of features to help users find the support they need, from a detailed therapist profile and appointment scheduling to resources and tools for mental well-being.",
@@ -57,8 +48,7 @@ export const projects = [
     team: [
       {
         name: "Jorge Rosano",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/jrh.jpg",
+        profilePicUrl: "/images/jrh.jpg",
         githubUrl: "https://github.com/jorge-ross",
       },
     ],
@@ -66,10 +56,8 @@ export const projects = [
   },
   {
     id: "3",
-    imgSrc:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/clivia.png",
-    imgComp:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/cli-comp.png",
+    imgSrc: "/images/clivia.png",
+    imgComp: "/images/cli-comp.png",
     title: "CLIvia Generator",
     summary:
       "The classic trivia game that challenges users with questions on a wide range of topics, including history, science, and pop culture. Players earn one point for each correct answer, and the highest score at the end of the game wins. Built with Ruby, this game provides an engaging and educational experience for users of all ages. Its dynamic interface and diverse question pool ensure that players stay entertained and informed throughout the gameplay.",
@@ -77,8 +65,7 @@ export const projects = [
     team: [
       {
         name: "Jorge Rosano",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/jrh.jpg",
+        profilePicUrl: "/images/jrh.jpg",
         githubUrl: "https://github.com/jorge-ross",
       },
     ],
@@ -86,10 +73,8 @@ export const projects = [
   },
   {
     id: "4",
-    imgSrc:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/doable-one.png",
-    imgComp:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/doable.png",
+    imgSrc: "/images/doable-one.png",
+    imgComp: "/images/doable.png",
     title: "Doable",
     summary:
       "A streamlined iteration of a note-taking application constructed using JavaScript, enabling users to generate, categorize, and oversee tasks. This application offers a user-friendly interface and efficient organization features, allowing users to create notes quickly, assign categories for better organization, and manage their tasks with ease. Whether for personal use or professional project management, this note-taking app enhances productivity and keeps users organized.",
@@ -97,8 +82,7 @@ export const projects = [
     team: [
       {
         name: "Jorge Rosano",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/jrh.jpg",
+        profilePicUrl: "/images/jrh.jpg",
         githubUrl: "https://github.com/jorge-ross",
       },
     ],
@@ -106,10 +90,8 @@ export const projects = [
   },
   {
     id: "5",
-    imgSrc:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/eatable.png",
-    imgComp:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/eatable-co.png",
+    imgSrc: "/images/eatable.png",
+    imgComp: "/images/eatable-co.png",
     title: "Eatable",
     summary:
       "A dynamic digital menu crafted with React for both personal and commercial use, allowing users to explore, create, edit, and delete dishes. This application provides a personalized user experience by enabling customization and management of menu items with ease. Whether for a home cook or a restaurant owner, this digital menu enhances the dining experience by offering flexibility and control over menu content.",
@@ -117,8 +99,7 @@ export const projects = [
     team: [
       {
         name: "Jorge Rosano",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/jrh.jpg",
+        profilePicUrl: "/images/jrh.jpg",
         githubUrl: "https://github.com/jorge-ross",
       },
     ],
@@ -126,10 +107,8 @@ export const projects = [
   },
   {
     id: "6",
-    imgSrc:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/b-ui.png",
-    imgComp:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/blog-co.png",
+    imgSrc: "/images/b-ui.png",
+    imgComp: "/images/blog-co.png",
     title: "Blog UI",
     summary:
       "A website focused on enhancing the user experience when interacting with an online blog. The objective of this project is to create an enjoyable and engaging user interface (UI) using HTML and CSS. By prioritizing intuitive design and aesthetic appeal, this website aims to make reading and navigating the blog a pleasant and seamless experience for users. Thoughtful design elements and responsive layouts are incorporated to ensure that the blog is visually appealing and easy to use across different devices and screen sizes.",
@@ -137,8 +116,7 @@ export const projects = [
     team: [
       {
         name: "Jorge Rosano",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/jrh.jpg",
+        profilePicUrl: "/images/jrh.jpg",
         githubUrl: "https://github.com/jorge-ross",
       },
     ],
@@ -146,10 +124,8 @@ export const projects = [
   },
   {
     id: "7",
-    imgSrc:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/contactable.png",
-    imgComp:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/conco.png",
+    imgSrc: "/images/contactable.png",
+    imgComp: "/images/conco.png",
 
     title: "Contactable",
     summary:
@@ -158,14 +134,12 @@ export const projects = [
     team: [
       {
         name: "Kevin Robles",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/kr.png",
+        profilePicUrl: "/images/kr.png",
         githubUrl: "https://github.com/kevinronu",
       },
       {
         name: "Jorge Rosano",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/jrh.jpg",
+        profilePicUrl: "/images/jrh.jpg",
         githubUrl: "https://github.com/jorge-ross",
       },
     ],
@@ -173,10 +147,8 @@ export const projects = [
   },
   {
     id: "8",
-    imgSrc:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/tweetable.png",
-    imgComp:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/tcom.png",
+    imgSrc: "/images/tweetable.png",
+    imgComp: "/images/tcom.png",
     title: "Tweetable",
     summary:
       "A simplified version of X, formerly known as Twitter, developed using Ruby on Rails. In this platform, users can create, reply to, and like posts, with the objective of creating a minimum viable product (MVP) of the X app. This project focuses on delivering the core functionalities of the original platform, offering a streamlined and efficient user experience while showcasing the essential features that make social interaction and content sharing easy and engaging.",
@@ -184,8 +156,7 @@ export const projects = [
     team: [
       {
         name: "Jorge Rosano",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/jrh.jpg",
+        profilePicUrl: "/images/jrh.jpg",
         githubUrl: "https://github.com/jorge-ross",
       },
     ],
@@ -193,10 +164,8 @@ export const projects = [
   },
   {
     id: "9",
-    imgSrc:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/critics.png",
-    imgComp:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/critics-comp.png",
+    imgSrc: "/images/critics.png",
+    imgComp: "/images/critics-comp.png",
     title: "Critics RC",
     summary:
       "An interactive video game catalog where users can sign up and log in with their username and password if already registered, granting access to reviews and ratings for each video game and company in the database. Additionally, users can create, edit, and delete their own companies and critiques. This platform offers a comprehensive and user-friendly experience, enabling gamers to explore detailed information, share their opinions, and manage their contributions, creating a vibrant community of video game enthusiasts.",
@@ -204,26 +173,22 @@ export const projects = [
     team: [
       {
         name: "Kevin Quispe",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/kq.jpg",
+        profilePicUrl: "/images/kq.jpg",
         githubUrl: "https://github.com/Kevincarlosqa",
       },
       {
         name: "Jorge Rosano",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/jrh.jpg",
+        profilePicUrl: "/images/jrh.jpg",
         githubUrl: "https://github.com/jorge-ross",
       },
       {
         name: "Gerardo Avena",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/gav.jpg",
+        profilePicUrl: "/images/gav.jpg",
         githubUrl: "https://github.com/Avenator02",
       },
       {
         name: "Sebastián Maguiña",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/sm.jpg",
+        profilePicUrl: "/images/sm.jpg",
         githubUrl: "https://github.com/Sebas54318",
       },
     ],
@@ -232,10 +197,8 @@ export const projects = [
   },
   {
     id: "10",
-    imgSrc:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/G-s-1.png",
-    imgComp:
-      "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/stats.png",
+    imgSrc: "/images/G-s-1.png",
+    imgComp: "/images/stats.png",
     title: "GitHub Stats",
     summary:
       "Investigate the primary statistical attributes of the GitHub community and preserve notable users for future reference with the GitHub Stats application. This powerful tool allows users to explore key metrics and trends within the GitHub community, offering valuable insights into user activity, repository growth, and project popularity. By identifying and saving notable users, the GitHub Stats application helps users stay informed, connected, and engaged within the development community, making it easier to track influential contributors and follow significant projects over time.",
@@ -243,14 +206,12 @@ export const projects = [
     team: [
       {
         name: "Jorge Rosano",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/jrh.jpg",
+        profilePicUrl: "/images/jrh.jpg",
         githubUrl: "https://github.com/jorge-ross",
       },
       {
         name: "Gabriel Nuñez",
-        profilePicUrl:
-          "https://github.com/jorge-ross/the-portfolio/raw/main/src/assets/images/gab.jpg",
+        profilePicUrl: "/images/gab.jpg",
         githubUrl: "https://github.com/NunezGabriel",
       },
     ],
